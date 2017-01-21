bc4d84d39a59fa3ef0c95140b87192f6405f0ccc
Leonardo Gatica <lgatica@protonmail.com>
2017-01-21 17:13:41 -0300

    chore(script): Remove port and unused if statement

--- run.sh
+++ run.sh
@@ -2,17 +2,11 @@
 
 set +e
 
-if [[ ${MONGO_1_PORT_27017_TCP_ADDR} ]]; then
-  echo "MONGO_1_PORT_27017_TCP_ADDR"
-  URI="mongodb://$MONGO_1_PORT_27017_TCP_ADDR:$MONGO_1_PORT_27017_TCP_PORT/agenda"
-elif [[ ${MONGO_PORT_27017_TCP_ADDR} ]]; then
-  echo "MONGO_PORT_27017_TCP_ADDR"
+if [[ ${MONGO_PORT_27017_TCP_ADDR} ]]; then
   URI="mongodb://$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/agenda"
 elif [[ ${MONGODB_URI} ]]; then
-  echo "MONGODB_URI"
   URI=$MONGODB_URI
 else
-  echo "local"
   URI="mongodb://localhost:27017/agenda"
 fi
 
@@ -22,10 +16,6 @@ else
   COLLECTION="agendaJobs"
 fi
 
-if [[ ${MONGODB_PORT} ]]; then
-  PORT=$MONGODB_PORT
-else
-  PORT=3000
-fi
+echo "agendash --db=$URI --collection=$COLLECTION"
 
-agendash --db=$URI --collection=$COLLECTION --port=$PORT
+agendash --db=$URI --collection=$COLLECTION
